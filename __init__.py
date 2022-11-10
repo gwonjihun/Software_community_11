c863855b384d462a383f9278ed344333c2300aac
gwonjihun <aspire51@naver.com>
2022-11-10 11:48:31 +0900

    queryupdate

--- __init__.py
+++ __init__.py
@@ -4,20 +4,60 @@ from model import Customer_DAO
 
 
 app = Flask(__name__)
-@app.route('/ckregister', method=['POST'])
-def check_register():
-    if request.mothed=='POST':
+
+@app.route('/register/check/id', methods=['POST'])
+def check_register_userid():
+    if request.method=='POST':
         if Customer_DAO.duplicate_member(request.form['userid']):
-            result = {'duplicate : True'}
+            result = {'duplicate' : 'True'}
             return make_response(jsonify(result),201)
         else:
-            reuslt = {'duplicate : False'}
+            result = {'duplicate' : 'False'}
             return make_response(jsonify(result,201))
 
+@app.route('/register/check/nickname', methods=['POST'])
+def check_register_nickname():
+    if session['user_id']:
+        if request.method=='POST':
+            if Customer_DAO.duplicate_nickname(request.form['user_nickname']):
+                result = {'duplicate' : 'True'}
+                return make_response(jsonify(result),201)
+            else:
+                result = {'duplicate' : 'False'}
+                return make_response(jsonify(result,201))
+    else:
+        return redirect('/',200)
+
+# @app.route('/mypage/update', methods=['POST'])
+#     # if session['user_id']:
+#     #     if request.
+#     # else:
+#     #     return redirect('/')
+
+# get -> 마이페이지 출력
+# post ->user정보 수정
+@app.route('/info',methods=['GET','POST'])
+def mypage():
+    if session['user_id']:
+        if request.method =='POST':
+            print()
+        elif request.method =='GET':
+            print()
+            return render_template('mypage.html')
+    else:
+        #  세션이 존재 x이면 닮은꼴 입력하는 초기 화면으로 진행한다.
+        return redirect('/')
 @app.route('/register',methods=['GET','POST'])
 def register():
     print(request.form)
     if request.method == 'POST':
+        id = request.form['user_id']
+        if id.isalnum() == False:
+            result = {
+                'space' : True,
+                'msg ' : '공백 문자 존재'
+            }
+            return make_response(jsonify(result),401)
         if Customer_DAO.duplicate_member(request.form['userid']):
             password =  generate_password_hash(request.form['password'])
             if Customer_DAO.register_member(request.form['userid'],password,request.form['user_nicname'],request.form['user_fullname'],request.form['birthday'],request.form['paper_flag'],request.form['gender_flag'],request.form['create_at']) :
@@ -30,17 +70,19 @@ def register():
 @app.route('/login',methods=['GET','POST'])
 def login():
     print(request.form)
+    if session['user_id']:
+        return render_template('main.html')
     if request.method == 'POST':
         password = request.form['password']
         if Customer_DAO.check_login(request.form['username'],password):
             print(request.form)
+            session['user_id'] = request.form['username']
             return '로그인 성공'
         else:
             return '로그인 실패'
     return render_template('login.html',msg='testing now')
 
-def logins():
-    print('git branch test')
+@ap
 # @app.route('/notice/share',methods=['GET','POST'])
 # def share_notice():
 #     pass
@@ -49,11 +91,29 @@ def logins():
 # def free_notice():
 #     pass
 
-# @app.route('/mypage/',methods=['GET','POST'])
+# @app.route('/mypage/',methods=['GET','POST','DELETE','UPDATE'])
 # def my_page():
 #     pass
 
+@app.route('/file_upload',methods = ['GET','POST'])
+def file():
+    return '''<form action="/file_upload" method="POST" enctype = "multipart/form-data">
+            <input type = "file" name = "file" />            
+            <input type='submit'>
+            '''
+@app.route('/file_upload',methods = ['GET','POST'])
+def file():
+    if request.method == 'POST':
+        file = request.files['file']
+        f = file.read()
+        name = file.filename
+        with open(name,'wb') as F:
+            F.write(f)
+        return redirect(main)
 
-
+    return '''<form action="/file_upload" method="POST" enctype = "multipart/form-data">
+            <input type = "file" name = "file" />            
+            <input type='submit'>
+            '''
 if __name__== '__main__':
     app.run(debug = True, port=8080)
